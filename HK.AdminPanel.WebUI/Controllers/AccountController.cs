b581dfc12c000e6f9663767c12c2351ab5361012
Hamed Kaboodjame <kaboodjame@live.com>
2024-04-13 20:50:09 +0330

    Add project files.

---
+++ HK.AdminPanel.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using HK.AdminPanel.WebUI.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HK.AdminPanel.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginRequest loginRequest)
+        {
+            return View(loginRequest);
+        }
+    }
+}
